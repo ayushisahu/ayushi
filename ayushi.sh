d48fc73d148b6398e57991e2f8f019cafd5f5ca5
ayushisahu <42178259+ayushisahu@users.noreply.github.com>
2018-08-14 16:31:15 +0530

    Add files via upload

---
+++ ayushi.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#This is a shell script
+#The art is called shell scripting
+echo "what is your name?"
+read AYUSHI
+mkdir $AYUSHI
+cd $AYSUHI
+touch AYUSHI.cpp
+
+
---
+++ ayushi1.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#This is a shell script
+#The art is called shell scripting
+echo "what is your name?"
+read ayushi
+mkdir $ayushi
+cd $ayushi
+touch ayushi1.cpp
+touch ayushi2.cpp
+touch ayushi3.cpp
+touch ayushi4.cpp
+touch ayushi5.cpp
