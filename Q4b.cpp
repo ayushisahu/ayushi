55a4859076a3dd941bbbbb8aebb2d1fb23f7fd33
ayushisahu <42178259+ayushisahu@users.noreply.github.com>
2018-09-08 08:02:48 +0000

    Add files via upload

---
+++ Q4b.cpp
@@ -0,0 +1,24 @@
+//program to find minimum between two numbers
+//include library
+#include<iostream>
+using namespace std;
+int min(int a,int b){
+   if (a<b)
+   return a;
+   else if (a==b)
+   cout<<"both are equal"<<endl;
+   else(a>b);
+   return b;}
+//use void
+void min(int a,int b, int &c){
+     cout<<min(a,b);}
+//define the driver function
+int main(){
+   int a,b;{
+   cout<<"input first no.";
+   cin>>a;
+   cout<<"input second no.";
+   cin>>b;
+   cout<<min(a,b)<<endl;
+   }
+return 0;}
